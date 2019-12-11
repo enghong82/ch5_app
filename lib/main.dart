83f8cc3144033f005b6fae3943c1dd50c19e366f
enghong82 <enghong82@gmail.com>
2019-12-11 17:51:04 +0800

    First commit

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+
+import 'package:flutter/material.dart';
+import 'package:ch5_app/pages/home.dart';
+
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Starter Template',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Home(),
+    );
+  }
+}
